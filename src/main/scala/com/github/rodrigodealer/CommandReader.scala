fc21b9e71c376b39002cb8fce079ec81b4179181
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-06-27 10:11:20 -0300

    Small enhancements

--- src/main/scala/com/github/rodrigodealer/CommandReader.scala
+++ src/main/scala/com/github/rodrigodealer/CommandReader.scala
@@ -10,13 +10,33 @@ import com.github.rodrigodealer.CommandType.Read
  * Created by rodrigo on 6/27/15.
  */
 object CommandReader {
-  def apply(command: String) = {
-    command match {
-      case op if op.contains("->") => Command(Post, op.split("->").head.trim, Option.empty, Option(op.split("->").last.trim))
-      case op if op.contains("follows") => Command(Follow, op.split("follows").head.trim, Option(op.split("follows").last.trim), Option.empty)
-      case op if op.contains("wall") => Command(Wall, op.split("wall").head.trim, Option.empty, Option.empty)
-      case op => Command(Read, op.trim, Option.empty, Option.empty)
-    }
+  val PostCommand = "->"
+  val FollowCommand = "follows"
+  val WallCommand = "wall"
+
+  def apply(command: String) = command match {
+    case op if op.isPost => Command(Post, user(op, Post), Option.empty, otherUser(command, Post))
+    case op if op.isFollow => Command(Follow, user(op, Follow), otherUser(command, Follow), Option.empty)
+    case op if op.isWall => Command(Wall, user(op, Wall), Option.empty, Option.empty)
+    case op => Command(Read, op.trim, Option.empty, Option.empty)
+  }
+
+  private[this] def otherUser(command: String, operation: CommandType) : Option[String] = operation match {
+    case Post => Option(command.split(PostCommand).last.trim)
+    case Follow => Option(command.split(FollowCommand).last.trim)
+    case _ => Option.empty
+  }
+
+  private[this] def user(command: String, operation: CommandType) = operation match {
+    case Post => command.split(PostCommand).head.trim
+    case Follow => command.split(FollowCommand).head.trim
+    case Wall => command.split(WallCommand).head.trim
+  }
+
+  private[this] implicit class StringCommands(s: String) {
+    def isPost = s.contains(PostCommand)
+    def isFollow = s.contains(FollowCommand)
+    def isWall = s.contains(WallCommand)
   }
 }
 
