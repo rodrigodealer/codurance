b2be02184a8312e2d7d184a227a98097bdf4a630
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-06-27 09:52:51 -0300

    Command Specs

---
+++ src/test/scala/com/github/rodrigodealer/CommandReaderSpec.scala
@@ -0,0 +1,22 @@
+package com.github.rodrigodealer
+
+import org.scalatest.FlatSpec
+
+/**
+ * Created by rodrigo on 6/26/15.
+ */
+class CommandReaderSpec extends FlatSpec {
+
+  it should "identify as a post command" in {
+
+  }
+
+  it should "identify as a read command" in {
+
+  }
+
+  it should "identify as a follow command" in {
+    
+  }
+
+}
