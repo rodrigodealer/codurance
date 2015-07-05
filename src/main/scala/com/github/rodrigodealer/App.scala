931a1f3e7e32d07b8a48d1b06efb3951b9f7159f
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-07-05 14:44:37 -0300

    Now calling services in main app, based in command type

--- src/main/scala/com/github/rodrigodealer/App.scala
+++ src/main/scala/com/github/rodrigodealer/App.scala
@@ -1,6 +1,10 @@
 package com.github.rodrigodealer
 
+import java.util.Date
+
 import com.github.rodrigodealer.CommandType._
+import com.github.rodrigodealer.models.{User, Message}
+import com.github.rodrigodealer.services.{MessageService, UserService}
 
 import scala.io.StdIn
 
@@ -8,17 +12,21 @@ import scala.io.StdIn
  * Created by rodrigo on 7/1/15.
  */
 object App {
+
   def main(args: Array[String]) {
     for (ln <- io.Source.stdin.getLines()) readCommandLine(ln)
   }
 
+  private val userService : UserService = new UserService
+  private val messageService : MessageService = new MessageService(userService)
+
   def readCommandLine(commandLine: String) = {
     val command = CommandReader(commandLine)
     command.operation match {
-      case Post => println(command.operation)
-      case Follow => println(command.operation)
-      case Read => println(command.operation)
-      case Wall => println(command.operation)
+      case Post => messageService.post(Message(command.args.get, User(command.user)))
+      case Follow => userService.follow(User(command.user), User(command.otherUser.get))
+      case Read => messageService.messagesFor(User(command.user)).foreach(m => println(m.format(new Date)))
+      case Wall => messageService.messagesOrderedByDateFor(User(command.user)).foreach(m => println(m.wallFormat(new Date)))
     }
   }
 }
