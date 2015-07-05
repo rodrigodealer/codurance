8360b5149b88cdc24e386339b79c2464cbaf389d
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-07-05 11:16:29 -0300

    MessageService and spec file

---
+++ src/main/scala/com/github/rodrigodealer/services/MessageService.scala
@@ -0,0 +1,8 @@
+package com.github.rodrigodealer.services
+
+/**
+ * Created by rodrigo on 7/5/15.
+ */
+class MessageService {
+
+}
---
+++ src/test/scala/com/github/rodrigodealer/services/MessageServiceSpec.scala
@@ -0,0 +1,10 @@
+package com.github.rodrigodealer.services
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by rodrigo on 7/5/15.
+ */
+class MessageServiceSpec extends FlatSpec with Matchers {
+
+}
