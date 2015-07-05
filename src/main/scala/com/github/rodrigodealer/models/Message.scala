c93772ffc9734b4c0e9e711edfa76088c315a05e
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-07-05 14:38:23 -0300

    Adding methods in Message that will format in wall format and timeline format

--- src/main/scala/com/github/rodrigodealer/models/Message.scala
+++ src/main/scala/com/github/rodrigodealer/models/Message.scala
@@ -8,5 +8,9 @@ import com.github.rodrigodealer.utils.DateUtil
  * Created by rodrigo on 7/5/15.
  */
 case class Message(content: String, user: User, date: Date = new Date) {
-  def postDate = DateUtil.elapsedTimeFrom(date, new Date)
+  def postDate(endDate: Date) = DateUtil.elapsedTimeFrom(date, endDate)
+
+  def format(endDate: Date) = s"$content (${postDate(endDate)})"
+
+  def wallFormat(endDate: Date) = s"${user.name} - $content (${postDate(endDate)})"
 }
---
+++ src/test/scala/com/github/rodrigodealer/models/MessageSpec.scala
@@ -0,0 +1,25 @@
+package com.github.rodrigodealer.models
+
+import org.joda.time.DateTime
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * Created by rodrigo on 7/5/15.
+ */
+class MessageSpec extends FlatSpec with Matchers {
+
+  it should "format message to a readable format" in {
+    val endDate = new DateTime(2015, 5, 26, 12, 35, 0, 0).toDate
+    val message = Message("I love summer", User("Johnny"), new DateTime(1980, 3, 26, 12, 35, 0, 0).toDate)
+
+    message.format(endDate) shouldBe "I love summer (35 years ago)"
+  }
+
+  it should "format wall message to a readable format" in {
+    val endDate = new DateTime(2015, 5, 26, 12, 35, 0, 0).toDate
+    val message = Message("I love summer", User("Johnny"), new DateTime(1980, 3, 26, 12, 35, 0, 0).toDate)
+
+    message.wallFormat(endDate) shouldBe "Johnny - I love summer (35 years ago)"
+  }
+
+}
