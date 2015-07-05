0d51aba293c31acef54b0cb3d35566612564ec7b
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-07-05 14:39:47 -0300

    Adding user to follow to an updated user

--- src/main/scala/com/github/rodrigodealer/services/UserService.scala
+++ src/main/scala/com/github/rodrigodealer/services/UserService.scala
@@ -10,7 +10,7 @@ class UserService {
   var users = Set[User]()
 
   def find(user: User) : Option[User] = {
-    users.filter(u => u.equals(user)) match {
+    users.filter(u => u.name.equals(user.name)) match {
       case userList if userList.size > 0 => Option(userList.head)
       case _ => Option.empty
     }
@@ -21,7 +21,7 @@ class UserService {
     user
   }
 
-  def follow(user: User, userToFollow: User) = {
-    user.follow(userToFollow)
+  def follow(user: User, userToFollow: User) {
+    find(user).get.follow(userToFollow)
   }
 }
